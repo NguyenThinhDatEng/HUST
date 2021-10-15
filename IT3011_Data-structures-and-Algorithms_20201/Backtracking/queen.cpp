0c4743c9e68dc673cc704b702886eb2515bae2a0
NguyenThinhDatSci <toithienamxinnghe@gmail.com>
2021-10-15 22:50:44 +0700

    new file

---
+++ IT3011_Data-structures-and-Algorithms_20201/Backtracking/queen.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string.h>
+#include<math.h>
+using namespace std;
+
+int* x;
+int n;
+
+void display()
+{
+    for (int i = 0; i < 8; i++)
+        cout << x[i] + 1 << " ";
+    cout << endl;
+}
+
+bool isInvalid(int i, int j)
+{
+    for (int k = 0; k < j; k++)
+    {
+        if (x[k] == i)
+            return false;
+        if (k - x[k] == j - i)
+            return false;
+        if (k + x[k] == i + j)
+            return false;
+    }
+    return true;
+}
+
+void TRY(int j){
+    for (int i = 0; i < 8; i++)
+    {
+        if (isInvalid(i, j))
+        {
+            x[j] = i;
+            if (j == 7)
+                display();
+            else
+                TRY(j + 1);
+            x[j] = -1;
+        }    
+    }
+}
+
+int main()
+{
+    x = new int[n];
+    TRY(0);
+    return 0;
+}
