299a4ce0a3ea880cd32fdbdd857c7fa816331aae
NguyenThinhDatSci <toithienamxinnghe@gmail.com>
2021-10-18 15:44:09 +0700

    Completed

--- IT3170_Application-of-Algorithms_20211/Training_2-DST-20211/Array.cpp
+++ IT3170_Application-of-Algorithms_20211/Training_2-DST-20211/Array.cpp
@@ -9,22 +9,28 @@ void solve(vector<int> negative, vector<int> positive)
 {
     cout << '1' << " " << negative[0] << endl;
     
-    if (positive.size() == 0)
-    {
-        cout << negative.size() - 1;
-        for (int i = 1; i < negative.size(); i++)
-            cout << " " << negative[i];
-    }
-    else
+    if (negative.size() % 2)
     {
         cout << negative.size() - 1 + positive.size();
         for (int i = 1; i < negative.size(); i++)
             cout << " " << negative[i];
         for (int i = 0; i < positive.size(); i++)
             cout << " " << positive[i];
+
+        cout << endl << 1 << " " << '0';
     }
+    else
+    {
+        cout << negative.size() - 2 + positive.size();
+        for (int i = 2; i < negative.size(); i++)
+            cout << " " << negative[i];
+        for (int i = 0; i < positive.size(); i++)
+            cout << " " << positive[i];
+
+        cout << endl << 2 << " " << '0' << " " << negative[1];
+    }
+
     
-    cout << endl << 1 << " " << '0';
 }
 
 int main()
