81fd8f1eeb68649bc7021b775115c092e4b034f1
NguyenThinhDatSci <toithienamxinnghe@gmail.com>
2021-10-21 13:21:20 +0700

    60%

--- IT3170_Application-of-Algorithms_20211/Training_3-EXHSEARCH-20211/K-CNK.cpp
+++ IT3170_Application-of-Algorithms_20211/Training_3-EXHSEARCH-20211/K-CNK.cpp
@@ -3,19 +3,19 @@
 #include<vector>
 
 using namespace std;
-unsigned long long** a;
+vector<vector<long long>> cnk;
 long long m;  // m <= 10^18
 long long halfN;
 
-unsigned long long C(int n, int k)
+long long C(int n, int k)
 {
     if (k > halfN)
         k = n - k;
-    if (k == n || k == 0) return 1;
-	if (k == 1) return n;
-	if (a[n][k] == 0)
-	    a[n][k] = C(n - 1, k - 1) + C(n - 1, k);
-	return a[n][k];
+    if (k == 0 || k == n) return 1 % m;
+	if (k == 1) return n % m;
+	if (cnk[n][k] == 0)
+	    cnk[n][k] = C(n - 1, k - 1) % m + C(n - 1, k) % m;
+	return cnk[n][k] % m;
 }
 
 int main()
@@ -23,20 +23,21 @@ int main()
     int T;  // T <= 10
     cin >> T;
     long long n;    // n <= 10^18
-    int k;  // k <= 10^5
+    int k;          // k <= 10^5
     while (T > 0)
     {
         cin >> n >> k;
         cin >> m;
         halfN = n / 2;
-        a = new unsigned long long*[n + 1];
+        cnk.resize(n + 1);
         for (long long i = 0; i <= n; i++)
-            a[i] = new unsigned long long[k + 1];
+            cnk[i].resize(k + 1);
 
         for (long long i = 0; i <= n; i++)
             for (int j = 0; j <= k; j++)
-                a[i][j] = 0;
-        cout << C(n, k) % m << endl;
+                cnk[i][k] = 0;
+
+        cout << C(n, k) << endl;
         T--;
     }
     return 0;
