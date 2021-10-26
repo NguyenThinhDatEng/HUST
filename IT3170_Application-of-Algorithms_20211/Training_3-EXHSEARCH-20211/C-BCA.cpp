41f8954d64d7192cdc145944bac08a39016da769
NguyenThinhDatSci <toithienamxinnghe@gmail.com>
2021-10-26 18:16:07 +0700

    fixing bug...

--- IT3170_Application-of-Algorithms_20211/Training_3-EXHSEARCH-20211/C-BCA.cpp
+++ IT3170_Application-of-Algorithms_20211/Training_3-EXHSEARCH-20211/C-BCA.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 #include<string.h>
 #include<math.h>
+#include<map>
 using namespace std;
 
 int m, n;   // teachers, courses
@@ -10,62 +11,93 @@ vector<vector<int>> preferenceList;
 vector<vector<int>> pairsOfConflicting;
 vector<vector<int>> trace;
 vector<int> existed;
-int course;
-int maxLoad;
+int tmp;
+int maxLoad = 0;
 int minOfMaxLoad = INT_MAX;
+int sizeTrace;
+bool check;
 
-void TRY(int k, int previousCourse)
+void TRY(int k)
 {
     if (k > n)
     {
+        // display the courses of each teacher
+        // int i = 0;
+        // for (int i = 1; i < trace.size(); i++)
+        // {
+        //     cout << i << ": ";
+        //     for (int j = 0; j < trace[i].size(); j++)
+        //     {
+        //         cout << trace[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        // cout << endl;
+        int size;
+        for (vector<int> v : trace)
+        {
+            size = v.size();
+            maxLoad = max(maxLoad, size);
+        }
         minOfMaxLoad = min(minOfMaxLoad, maxLoad);
         return;
     }
 
-    // for (int i = 1; i <= m; i++)
-    // {
-    //     int size = preferenceList[i].size();
-    //     int maxLoad = -1;
-    //     int tmpMaxLoad = 0;
-    //     for (int j = 0; j < size; j++)
-    //     {
-    //         course = preferenceList[i][j];
-    //         if (existed[course] == 0 && pairsOfConflicting[previousCourse][course] != 1)
-    //         {
-    //             trace[i].push_back(course);
-    //             existed[course] = 1;
-    //             tmpMaxLoad++;
-    //             TRY(k + 1, course);
-    //         }
-    //     }
-    //     maxLoad = max(maxLoad, tmpMaxLoad);
-    // }
+    if (existed[k])
+    {
+        TRY(k + 1);
+    }
+
+    {
+        for (int i = 0; i < preferenceList[k].size(); i++)
+        {
+            check = true;
+            tmp = preferenceList[k][i]; // teacher
+            sizeTrace = trace[tmp].size();
+            for (int j = 0; j < sizeTrace; j++)
+            {
+                if (pairsOfConflicting.at(trace[tmp][j]).at(k) == 1)
+                {
+                    check = false;
+                    break;
+                }
+            }
+
+            if (check)
+            {
+                trace[tmp].push_back(k);
+                TRY(k + 1);
+                trace[tmp].pop_back();
+            }
+        }
+    }
 
 }
 
 int main()
 {
-    cin >> m >> n;  // 1 <= m <= 10, 1 <= n <=30
-    preferenceList.resize(m + 1);
-    trace.resize(m + 1);
-    existed.resize(n + 1);
+    cin >> m >> n;  // 1 <= m <= 10, 1 <= n <= 30
+    preferenceList.resize(n + 1);   // course -> teachers: 1 -> Van, Lisa
+    trace.resize(m + 1);            // teacher -> courses: Van -> 1, 3, 4
+    existed.resize(n + 1);          // mark courses that are only suitable for one teacher
 
+    // input
     int k;  // indicating the courses that teacher i can teach
-    int tmp;
+    int teacher;
     for (int i = 1; i <= m; i++)
     {
         cin >> k;
-        preferenceList[i].resize(k);
-        for (int j = 0; j < k; j++)
+        for (int j = 1; j <= k; j++)
         {
-            cin >> tmp;
-            preferenceList[i][j] = tmp;
+            cin >> teacher;
+            preferenceList[teacher].push_back(i);
         }
     }
 
+    // input
     cin >> k;   // number of pairs of conflicting two courses
-    pairsOfConflicting.resize(n + 1);
 
+    pairsOfConflicting.resize(n + 1);
     for (int i = 0; i <= n; i++)
         pairsOfConflicting[i].resize(n + 1);
 
@@ -77,8 +109,30 @@ int main()
         k--;
     }
 
-    TRY(1, 0);
+    // check courses that are only suitable for one teacher
+    for (int i = 1; i <= n; i++)
+    {
+        if (preferenceList[i].size() == 1)
+        {
+            teacher = preferenceList[i].back();
+            for (int j = 0; j < trace[teacher].size(); j++)
+            {
+                if (pairsOfConflicting.at(trace[teacher][j]).at(i) == 1)
+                {
+                    cout << -1;
+                    return 0;
+                }
+            }
+            
+            existed[i] = 1;
+            trace[teacher].push_back(i);
+        }
+    }
 
-    cout << 1;
+    TRY(1);
+    if (maxLoad == 0)
+        cout << -1;
+    else
+        cout << minOfMaxLoad;
     return 0;
 }
