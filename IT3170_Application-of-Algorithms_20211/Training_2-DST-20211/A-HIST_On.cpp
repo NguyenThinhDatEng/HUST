4d8a51a919ff869dbe623999d654c7186970893a
NguyenThinhDatSci <toithienamxinnghe@gmail.com>
2021-11-13 09:15:37 +0700

    convert type of maxS from int to long long

--- IT3170_Application-of-Algorithms_20211/Training_2-DST-20211/A-HIST_On.cpp
+++ IT3170_Application-of-Algorithms_20211/Training_2-DST-20211/A-HIST_On.cpp
@@ -9,12 +9,13 @@ using namespace std;
 
 void input(int& n, vector<int>& v)
 {
-    cin >> n;
+    cin >> n;   // 1≤n≤1000000
     int tmp;
+    // insert -1 to 2 local (front, back of v)
     v.push_back(-1);
     for (int i = 1; i <= n; i++)
     {
-        cin >> tmp;
+        cin >> tmp; //   0≤tmp≤100000000
         v.push_back(tmp);
     }
     v.push_back(-1);
@@ -24,34 +25,34 @@ int main()
 {
     ios_base :: sync_with_stdio (0);
     cin.tie (0);cout.tie (0);
-    int n = 1;
-    vector<int> v;
-    vector<int> L, R;
-    int maxS;
+    int n = 1;  // No. of elements
+    vector<int> v;  // contains all of elements
+    vector<int> L, R;   // contain index nearest lower column
+    long long maxS;
     while (n)
     {
         input(n, v);
         if (n == 0)
             break;
-        stack<int> left, right;
+        stack<int> left, right; // contain index temporary nearest lower column
         maxS = 0;
-
-        int tmp;
+        
+        // algorithm
         int leftIndex, rightIndex;
         for (int i = 1; i <= n; i++)
         {
             left.push(i - 1);
-            leftIndex = left.top();
-            while (v[i] < v[leftIndex])
+            leftIndex = left.top(); // get index
+            while (v[i] <= v[leftIndex])
             {
                 left.pop();
                 leftIndex = left.top();
             }
             L.push_back(leftIndex);
 
-            right.push(n - i + 2);
+            right.push(n + 2 - i);  // v has n + 2 elements (index of last element is n + 1)
             rightIndex = right.top();
-            while (v[n - i + 1] <= v[rightIndex])
+            while (v[n + 1 - i] <= v[rightIndex])
             {
                 right.pop();
                 rightIndex = right.top();
@@ -61,8 +62,11 @@ int main()
         
         for (int i = 0; i < n; i++)
         {
-            maxS = max(maxS, (R[n - i - 1] - L[i] - 1) * v[i + 1]);
+            long long maxTmp = 1LL * (R[n - i - 1] - L[i] - 1) * v[i + 1];
+            maxS = max(maxS, maxTmp);
         }
+
+        // display
         cout << maxS << endl;
         v.clear();
         L.clear();
