030d1f3ebcc44d7207ad2c2dd8f29f570c7b2685
NguyenThinhDatSci <toithienamxinnghe@gmail.com>
2021-11-15 20:59:03 +0700

    100%

--- IT3170_Application-of-Algorithms_20211/Training_2-DST-20211/K-Queue.cpp
+++ IT3170_Application-of-Algorithms_20211/Training_2-DST-20211/K-Queue.cpp
@@ -1,62 +1,94 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<string.h>
-#include<math.h>
-#include<deque>
-
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+#include <math.h>
+#include <iomanip>
+#include <set>
+#include <deque>
 using namespace std;
 
-void solved(deque<int> walruses, int n)
+int binarySearch(vector<int>& v, vector<int>& queue, int age, int l, int r)
 {
-    vector<int> indexOfMin;
-    int min = walruses[n - 1] + 1;
-    // for (int i = n - 1; i >= 0; i--)
-    // {
-    //     if (min >= walruses[i])
-    //     {
-    //         indexOfMin.push_back(i);
-    //         min = walruses[i];
-    //     }
-    // }
-
-    int i;
-    while (!walruses.empty())
+    if (r >= l)
     {
-        i = walruses.size() - 1;
-        while (i >= 0)
+        int mid = (r + l) / 2;
+        int indexOfWal = queue[mid];
+        if (age > v[indexOfWal] && age <= v[queue[mid - 1]])
         {
-            if (min >= walruses[i])
+            return indexOfWal;
+        }
+
+        if (age <= v[indexOfWal])
+            return binarySearch(v, queue, age, mid + 1, r);
+
+        return binarySearch(v, queue, age, l, mid - 1);
+    }
+    return -1;
+}
+
+void solve(int n, vector<int> v)
+{
+    vector<int> queue;  // save index of walruses
+    vector<int> rs;
+    int firstWal = v.back();
+
+    // push first walruse to queue
+    queue.push_back(n - 1);
+    rs.push_back(-1);
+
+    // algo
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (v[i] > firstWal)
+            rs.push_back(n - 1 - i - 1);
+        else
+        {
+            int r = queue.size() - 1;
+            int mid = binarySearch(v, queue, v[i], 1, r);
+            
+            if (mid == -1)
             {
-                // indexOfMin.push_back(i);
-                min = walruses[i];
-                if (walruses.front() > min)
-                {
-                    walruses.pop_front();
-                    cout << i - 1 << " ";
-                    break;
-                }
+                queue.push_back(i);
+                rs.push_back(-1);
             }
-            i--;
+            else
+            {
+                rs.push_back(mid - i - 1);
+            }
         }
     }
 
+    reverse(rs.begin(), rs.end());
+    for (int i = 0; i < n; i++)
+    {
+        cout << rs[i] << " ";
+    }
+}
+
+void input(int &n, vector<int> &v)
+{
+    cin >> n;
+    int tmp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tmp;
+        v.push_back(tmp);
+    }
 }
 
 int main()
 {
-    int t;
-    cin >> t;
-    deque<int> walruses;
+    ios_base ::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // instance variable
+    int n;         // No. of walruses, 2 ≤ n ≤ 10^5
+    vector<int> v; // list of walruses, 1 ≤ v[i] ≤ 10^9
 
-    int tmp;
-    for (int i = 0; i < t; i++)
-    {
-        cin >> tmp;
-        walruses.push_back(tmp);
-    }
+    input(n, v);
 
-    solved(walruses, t);
+    solve(n, v);
 
     return 0;
 }
