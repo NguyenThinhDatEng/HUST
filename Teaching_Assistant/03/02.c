5c342b93a6e3913b7daaf0741cae45c2ba16ddf6
NguyenThinhDatEng <toithienamxinnghe@gmail.com>
2022-05-31 08:28:30 +0700

    updated 02.c

--- Teaching_Assistant/03/02.c
+++ Teaching_Assistant/03/02.c
@@ -5,14 +5,14 @@ int main()
     double r;
     scanf("%lf", &r);
     if (r <= 0)
-    {
         printf("ERROR");
-        return 0;
+    else
+    {
+        float const PI = 3.14159;
+        double M, S;
+        M = 2 * PI * r;
+        S = PI * r * r;
+        printf("%lf\n%lf", M, S);
     }
-    float const PI = 3.14159;
-    double M, S;
-    M = 2 * PI * r;
-    S = PI * r * r;
-    printf("%lf\n%lf", M, S);
     return 0;
 }
