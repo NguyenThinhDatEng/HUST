ebe4c0764a9d27374585cf2f92c0822b76104991
NguyenThinhDatEng <thinh.nv194178@sis.hust.edu.vn>
2022-12-09 15:37:50 +0700

    20%

---
+++ IT4409_Web_Technology_And_Online_Services/freeCodeCamp/Palindrome_Checker.js
@@ -0,0 +1,38 @@
+const isAlphaNum = (c) => {
+  if ((c >= "a" && c <= "z") || (c >= "0" && c <= "9")) {
+    return true;
+  }
+  return false;
+};
+
+function palindrome(str) {
+  // Viết thường các ký tự trong chuỗi
+  str = str.toLowerCase();
+  // Tạo chuỗi kí tự chỉ gồm chữ và số từ str
+  let characters = "";
+  for (const c of str) {
+    if (isAlphaNum(c)) characters += c;
+  }
+  // Kiểm tra
+  const len = characters.length; // chiều dài chuỗi characters
+  for (let i = 0; i < len; i++) {
+    if (characters[i] != characters[len - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const words = [
+  "eye",
+  "_eye",
+  "race car",
+  "A man, a plan, a canal. Panama",
+  "My age is 0, 0 si ega ym.",
+  "0_0 (: /-\\ :) 0-0",
+  "five|\\_/|four",
+];
+
+for (const word of words) {
+  console.log(palindrome(word));
+}
